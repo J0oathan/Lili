8126d2e0d73dba1914c571cc4e37e59864f25a45
J0oathan <1302Jona@gmail.com>
2024-07-09 14:16:21 -0600

    Msj de Jona

--- Ejercicios/ejercicio1.js
+++ Ejercicios/ejercicio1.js
@@ -15,6 +15,8 @@ function getRandomNumber() {
   return Math.floor(Math.random() * colorsArray.length);
 }
 
+console.log('Mensaje de Jona');
+
 /*const COLORS = ["green", "red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
